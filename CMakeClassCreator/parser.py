58a1c76012c97a5e3c56d133e391983ad8f1c450
Frank <frankgoyens@hotmail.com>
2020-08-14 22:04:16 +0200

    Added 'add_library' statement to parser

--- CMakeClassCreator/parser.py
+++ CMakeClassCreator/parser.py
@@ -16,9 +16,29 @@ class Parser(object):
             + self._variable_name \
                 + SkipTo(self._parent_scope_keyword + ")" | ")", include=True, ignore=self._comment_stmt | dblQuotedString)
 
+        self._content_until_endparen = SkipTo(")", include=True, ignore=self._comment_stmt | dblQuotedString)
+
         #https://cmake.org/cmake/help/latest/command/set.html#set-environment-variable
         #We need to define this one to be able to ignore it
-        self._set_env_variable_stmt = self._set_keyword + "(" + Literal("ENV") + ")"
+        self._set_env_variable_stmt = self._set_keyword + "(" + Literal("ENV") \
+            + self._content_until_endparen
+
+        self._add_library_keyword = CaselessLiteral("add_library")
+        self._normal_library_types = Literal("STATIC") | Literal("SHARED") | Literal("MODULE")
+        self._library_name = self._variable_name
+
+        #https://cmake.org/cmake/help/latest/command/add_library.html#normal-libraries
+        self._add_library_stmt = self._add_library_keyword + "(" \
+            + self._library_name + Optional(self._normal_library_types) \
+                + Optional(Literal("EXCLUDE_FROM_ALL")) \
+                    + self._content_until_endparen
+
+        self._object_library_type = Literal("OBJECT")
+
+        #https://cmake.org/cmake/help/latest/command/add_library.html#object-libraries
+        self._add_object_library_stmt = self._add_library_keyword + "(" \
+            + self._library_name + self._object_library_type \
+                + self._content_until_endparen
 
         #Limited only to the things we actually need of course
         self._cmake_stmt = self._set_normal_variable_stmt
--- Test/testParser.py
+++ Test/testParser.py
@@ -28,12 +28,12 @@ class ParserTest(unittest.TestCase):
         result = givenParser._set_normal_variable_stmt.parseString(givenString)
         self.assertEqual(list(result), ["set", "(", "var", givenStringVarContent, ")"])
 
-    def test_normal_set_stmt_handle_garbage(self):
-        givenGarbageString = "432534^^*&^%*&2341029357*(&)())(_)(*&(*)(_!"
-
+    def test_normal_set_stmt_handle_wrong_similar(self):
         givenParser = Parser()
-        result = givenParser._set_normal_variable_stmt.searchString(givenGarbageString)
-        self.assertEqual(list(result), [])
+
+        self.assertEqual(list(givenParser._set_normal_variable_stmt.searchString("set(x t")), [])
+        self.assertEqual(list(givenParser._set_normal_variable_stmt.searchString("set x t)")), [])
+        self.assertEqual(list(givenParser._set_normal_variable_stmt.searchString("set_ (x t)")), [])
 
     def test_normal_set_stmt_with_parent_scope(self):
         givenString = "set(var content PARENT_SCOPE)"
@@ -48,7 +48,55 @@ class ParserTest(unittest.TestCase):
 
         result = givenParser._set_normal_variable_stmt.parseString(givenString)
         self.assertEqual(list(result), ["set", "(", "ENV", "\{ENV_VAR_NAME\} value", ")"])
+    
+    def test_add_library_stmt(self):
+        givenString = "add_library(TabsPlsLib main.cpp)"
+        givenParser = Parser()
 
+        result = givenParser._add_library_stmt.parseString(givenString)
+        self.assertEqual(list(result), ["add_library", "(", "TabsPlsLib", "main.cpp", ")"])
+    
+    def test_add_library_stmt_with_type(self):
+        givenString = "add_library(TabsPlsLib STATIC main.cpp)"
+        givenParser = Parser()
+
+        result = givenParser._add_library_stmt.parseString(givenString)
+        self.assertEqual(list(result), ["add_library", "(", "TabsPlsLib", "STATIC", "main.cpp", ")"])
+
+    def test_add_library_stmt_with_type(self):
+        givenString = "add_library(TabsPlsLib STATIC main.cpp)"
+        givenParser = Parser()
+
+        result = givenParser._add_library_stmt.parseString(givenString)
+        self.assertEqual(list(result), ["add_library", "(", "TabsPlsLib", "STATIC", "main.cpp", ")"])
+
+    def test_add_library_stmt_with_exclude_from_all(self):
+        givenString = "add_library(TabsPlsLib STATIC EXCLUDE_FROM_ALL main.cpp)"
+        givenParser = Parser()
+
+        result = givenParser._add_library_stmt.parseString(givenString)
+        self.assertEqual(list(result), ["add_library", "(", "TabsPlsLib", "STATIC", "EXCLUDE_FROM_ALL", "main.cpp", ")"])
+
+    def test_add_library_stmt_handle_wrong_similar(self):
+        givenParser = Parser()
+
+        self.assertEqual(list(givenParser._add_library_stmt.searchString("add_library(x t")), [])
+        self.assertEqual(list(givenParser._add_library_stmt.searchString("add_library x t)")), [])
+        self.assertEqual(list(givenParser._add_library_stmt.searchString("add_library_ (x t)")), [])
+
+    def test_add_object_library_stmt(self):
+        givenString = "add_library(TabsPlsLib OBJECT shared_component.hpp shared_component.cpp)"
+        givenParser = Parser()
+
+        result = givenParser._add_object_library_stmt.parseString(givenString)
+        self.assertEqual(list(result), ["add_library", "(", "TabsPlsLib", "OBJECT", "shared_component.hpp shared_component.cpp", ")"])
+
+    def test_add_object_library_stmt_handle_wrong_similar(self):
+        givenParser = Parser()
+
+        self.assertEqual(list(givenParser._add_object_library_stmt.searchString("add_library(x OBJECT t")), [])
+        self.assertEqual(list(givenParser._add_object_library_stmt.searchString("add_library x OBJECT t)")), [])
+        self.assertEqual(list(givenParser._add_object_library_stmt.searchString("add_library_ (x OBJECT t)")), [])
 
 if __name__ == '__main__':
      unittest.main()
