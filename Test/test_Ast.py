3bd0c3fd715d01b9981992de52bbccaac73bd1ee
Frank Goyens <frankgoyens@hotmail.com>
2020-08-30 16:01:41 +0200

    Added test to parse a variable usage in a set statement
    Had to correct use of curly braces in test_Parser.py

--- Test/test_Ast.py
+++ Test/test_Ast.py
@@ -13,5 +13,13 @@ class TestAst(unittest.TestCase):
 
         self.assertTrue(list(result)[0].is_same(expected_result))
 
+    def test_parse_set_normal_variable_using_other_variable(self):
+        givenAst = ast.Ast()
+        result = givenAst.parse("set(TabsPls_Source ${{other_var}})")
+
+        expected_result = ast.SetNormalVariable("TabsPls_Source", ast.CMakeStringList([ast.VariableUse("other_var")])) 
+
+        self.assertTrue(list(result)[0].is_same(expected_result))
+
 if __name__ == '__main__':
     unittest.main()
