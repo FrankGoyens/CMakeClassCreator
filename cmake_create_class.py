e0c78ca3ab9eb2ddf9744eaffa324650eb194a9b
Frank Goyens <frankgoyens@hotmail.com>
2020-10-17 19:23:18 +0200

    Set up argument parsing and validation
    Also added tests for argument validation

--- cmake_create_class.py
+++ cmake_create_class.py
@@ -1,7 +1,48 @@
-from CMakeClassCreator.parser import Parser
+import argparse
 
-s = ("set(var content)"
-"#set(actualvar actualcontent)")
-parser = Parser()
-result = parser._cmake_stmt.searchString(s)
-print(result)
+from collections import namedtuple
+
+class CMakeClassCreatorException(Exception):
+    pass
+
+def create_arg_parser():
+    parser = argparse.ArgumentParser(description="Create a new class by modifying CMake scripts.")
+    parser.add_argument("cmakelists", help="The cmake script where the class will be added.")
+    parser.add_argument("name", help="The name of the new class. In single file mode, this is the name of the file including file extension.")
+
+    parser.add_argument("-rc", "--reference-class", help="Add the new class in the same way the reference class was added.")
+    parser.add_argument("-i", "--inplace", action="store_true", help="Modify the cmake script in place instead of writing to stdout.")
+
+    parser.add_argument("-s", "--single-file", action="store_true", help="Single file mode, only add a single file.")
+    parser.add_argument("-var", "--variable", help="Implies single file mode. Add the source file to the given cmake variable.")
+    parser.add_argument("-t", "--target", help="Implies single file mode. Add the source file to the given cmake target (library or executable).")
+    return parser
+
+def validate_args(args):
+    if using_single_file_mode(args):
+        validate_args_single_file_mode(args)
+    else:
+        validate_args_class_mode(args)
+
+def using_single_file_mode(args):
+    return args.single_file or args.variable or args.target
+
+def validate_args_single_file_mode(args):
+    if args.reference_class:
+        if args.variable or args.target:
+            raise CMakeClassCreatorException("In single file mode, it is not allowed to specify a reference class and also a variable or target.")
+    else:
+        if not args.variable and not args.target:
+            raise CMakeClassCreatorException("In single file mode, please specify a cmake variable or a cmake target using -var, --variable or -t, --target respectively.")
+        if args.variable and args.target:
+            raise CMakeClassCreatorException("In single file mode, it is not allowed to specify both a variable and a target.")
+
+def validate_args_class_mode(args):
+    if not args.reference_class:
+        raise CMakeClassCreatorException("When adding a class, a reference class is required. Please specify one with -rc, --reference-class.")
+
+if __name__ == "__main__":
+    args = create_arg_parser().parse_args()
+
+    validate_args(args)
+    print("Thank you for the valid arguments! All I can do right now is print this message, more functionality coming soon. Like adding {0} to {1}.".format(args.name, args.cmakelists))
