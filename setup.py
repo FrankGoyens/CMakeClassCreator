9adbce7e57fa988c0e787fb6ffa62f61a85844cd
Frank <frankgoyens@hotmail.com>
2020-12-03 21:59:17 +0100

    Created a setup.py
    Configured the readme and requirements to be used in the setup
    Also defined the entry point in setup.py, the top level script was made entry point friendly

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os
+from distutils.core import setup
+
+install_requires = []
+with open("requirements.txt", 'r') as requirements_file:
+      install_requires = requirements_file.read().splitlines()
+
+long_description = ""
+with open("README.md", 'r') as long_description_file:
+      long_description = long_description_file.read()
+
+setup(name='CMake-Class-Creator-FrankG',
+      version='0.1.0',
+      description='A script that inserts a new c++ class in an existing CMake configuration.',
+      author='Frank Goyens',
+      url='https://github.com/FrankGoyens/CMakeClassCreator',
+      py_modules=['cmake_create_class', "CMakeClassCreator"],
+      install_requires=install_requires,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      entry_points={"console_scripts": {"cmake_create_class=cmake_create_class:main"}}
+     )
