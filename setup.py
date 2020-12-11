566c34cc53ae3c9a3ad0fd2d7f44ecd67b4208f2
Frank <frankgoyens@hotmail.com>
2020-12-11 20:21:32 +0100

    Removed username from package name
    I don't think anyone does this

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = ""
 with open("README.md", 'r') as long_description_file:
       long_description = long_description_file.read()
 
-setuptools.setup(name='CMake-Class-Creator-FrankG',
+setuptools.setup(name='CMake-Class-Creator',
       version='0.1.0',
       description='A script that inserts a new c++ class in an existing CMake configuration.',
       author='Frank Goyens',
